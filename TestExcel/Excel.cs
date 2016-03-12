8c062aae3a7cf3f732d5c908aa440d4cf4ef0d8a
qioixiy <qioixiy@gmail.com>
2016-03-13 00:23:24 +0800

    init

---
+++ TestExcel/Excel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TestExcel
+{
+    public class CBExcel
+    {
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+
+        public CBExcel()
+        {
+        }
+
+        public void SetData(int i, int j, string data)
+        {
+            xlWorkSheet.Cells[i, j] = data;
+        }
+
+        public void SetChart(string start, string end, Excel.XlChartType type)
+        {
+            Excel.Range chartRange;
+
+            Excel.ChartObjects xlCharts = (Excel.ChartObjects)xlWorkSheet.ChartObjects(Type.Missing);
+            Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+            Excel.Chart chartPage = myChart.Chart;
+
+            chartRange = xlWorkSheet.get_Range(start, end);
+            chartPage.SetSourceData(chartRange, misValue);
+            chartPage.ChartType = type;
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        public void Create()
+        {
+            xlApp = new Excel.ApplicationClass();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+        }
+
+        public void SaveAs()
+        {
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+        }
+
+        public void Release()
+        {
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+    }
+}
---
+++ TestExcel/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace TestExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CBExcel excel = new CBExcel();
+            excel.Create();
+            excel.SetData(1, 1, "");
+            excel.SetData(1, 2, "Student1");
+            excel.SetData(1, 3, "Student2");
+            excel.SetData(1, 4, "Student3");
+
+            excel.SetData(2, 1, "Term1");
+            excel.SetData(2, 2, "80");
+            excel.SetData(2, 3, "65");
+            excel.SetData(2, 4, "45");
+
+            excel.SetData(3, 1, "Term2");
+            excel.SetData(3, 2, "81");
+            excel.SetData(3, 3, "61");
+            excel.SetData(3, 4, "41");
+
+            excel.SetData(4, 1, "Term3");
+            excel.SetData(4, 2, "82");
+            excel.SetData(4, 3, "62");
+            excel.SetData(4, 4, "42");
+
+            excel.SetChart("A1", "D4", Excel.XlChartType.xlLine);
+            excel.SaveAs();
+            excel.Release();
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
